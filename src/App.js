6204dc7da2a8dcb93b8e2336de42a95143cd3dab
EdebaliErman <edebalierman@gmail.com>
2023-11-20 02:40:43 +0300

    Bg

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ function App() {
   return (
     <div className="App">
       <img 
-      className='absolute w-full  z-0 bottom-[-3.5rem]'
+      className=' w-full  z-0 bottom-0 fixed xl:hidden'
       alt='bg'
       src={process.env.PUBLIC_URL+"assets/bg-desktop.png"} />
+          <img 
+      className=' w-full  z-0 bottom-0 fixed hidden xl:flex xl:left-0 xl:top-0 xl:h-full '
+      alt='bg'
+      src={process.env.PUBLIC_URL+"assets/bg-mobile.png"} />
       <CardFolder />
       <CardStat />
     </div>
