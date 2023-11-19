78d3e8856a38c93acdaaee65ab7cb631e270a8c7
EdebaliErman <edebalierman@gmail.com>
2023-11-19 18:07:26 +0300

    Svg try

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import CardFolder from './Components/CardFolder/CardFolder';
+import CardStat from './Components/CardStat/CardStat';
 
 function App() {
   return (
     <div className="App">
-     
+      <CardFolder />
+      <CardStat />
     </div>
   );
 }
--- src/Components/CardFolder/CardFolder.js
+++ src/Components/CardFolder/CardFolder.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import "./CardFolder.css"
+import { icons } from '../SVG/svgData'
 
 function CardFolder() {
   return (
-    <div>
-      
+    <div className='card-folder'>
+      <div className='logo'>
+      {icons.logo}
+      </div>
+      <div className='folder-box'>
+        {icons.folder}
+        {icons.upload}
+        {icons.sky}
+      </div>
     </div>
   )
 }
