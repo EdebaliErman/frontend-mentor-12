023010d4863f6b149a62c3f6ae7ad19a891beea1
EdebaliErman <edebalierman@gmail.com>
2023-11-19 21:27:01 +0300

    Boumble box

--- src/Components/CardStat/CardStat.js
+++ src/Components/CardStat/CardStat.js
@@ -2,8 +2,24 @@ import React from 'react'
 import "./CardStat.css"
 function CardStat() {
   return (
-    <div>
-      
+    <div className='cardStat'>
+      <div className='absolute top-[-3rem] right-10'>
+        <div className='speech-bubble'>
+          <h2 className='flex justify-center items-center relative z-10 text-black text-5xl font-extrabold font-serif'>185<span className=' mt-4 mx-3 font-sans text-xl font-bold text-gray-400'>GB LEFT</span></h2>
+        </div>
+      </div>
+      <h1>You've used <span className='text-white'>815 GB</span> of your stroge</h1>
+      <div className='stat-bar'>
+        <div className='skill-bar'>
+          <svg className=' skill'>
+          </svg>
+          <svg className='pointer'></svg>
+        </div>
+        <div className='text-bar'>
+          <h4>0 GB</h4>
+          <h4>1000 GB</h4>
+        </div>
+      </div>
     </div>
   )
 }
